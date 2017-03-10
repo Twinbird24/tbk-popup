f9837d3de4ccdb548a611e2b1c6418490a81f144
Attila Komaromi <attila.kom@gmail.com>
2017-03-10 18:37:19 -0500

    Initial commit

---
+++ popup.js
@@ -0,0 +1,6 @@
+// after 3 boring seconds of viewing the website, give the user summin' nice to look at
+setTimeout( (function(){ $('#pop-up').fadeIn('fast'); }), 3000);
+
+
+
+//$('#pop-up').fadeOut('fast');
