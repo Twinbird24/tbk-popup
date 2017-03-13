498c38a5ef0a6ccb24c56f4cb934847bca7f9936
Attila Komaromi <attila.kom@gmail.com>
2017-03-13 16:14:51 -0400

    Add function to prevent future pop-ups

--- popup.js
+++ popup.js
@@ -1,17 +1,29 @@
+// if the pop-up hasn't been viewed already, let's pop it up
+if(localStorage.popUpViewed == undefined) {
+	startPopUp();
+}
+
 // after 3 boring seconds of viewing the website, give the user summin' nice to look at
-setTimeout( (function() {
-	$('#pop-up-container').fadeIn('fast'); // fade in pop-up
+function startPopUp() {
+	setTimeout( (function() {
+		$('#pop-up-container').fadeIn('fast'); // fade in pop-up
 
-	// check if pop-up is scrolled and run associated handler
-	$('#pop-up-container').bind('scroll', function(e) {
-		parallaxScroll();
-	});
-	function parallaxScroll() {
-		var scrolledY = $('#pop-up-container').scrollTop();
-		// if you want to changed scroll speed, modify the multiplier (aka the *0.2 and *-0.2 numbers)
-		$('#chisel').css('top','+20' - (((scrolledY)*0.2)) + '%');
-		$('#shavings').css('top','+10' - (((scrolledY)*-0.2)) + '%');
-	}
-}), 1000);
+		// check if pop-up is scrolled and run associated handler
+		$('#pop-up-container').bind('scroll', function(e) {
+			parallaxScroll();
+		});
+		function parallaxScroll() {
+			var scrolledY = $('#pop-up-container').scrollTop();
+			// if you want to changed scroll speed, modify the multiplier (aka the *0.2 and *-0.2 numbers)
+			$('#chisel').css('top','+20' - (((scrolledY)*0.2)) + '%');
+			$('#shavings').css('top','+10' - (((scrolledY)*-0.2)) + '%');
+		}
+		// close the pop-up
+		$('#close-btn>p').click(function() {
+			$('#pop-up-container').fadeOut('fast');
+		});
+	}), 1000);
 
-//$('#pop-up').fadeOut('fast');
+	// set local storage to prevent future pop-ups
+	localStorage.setItem('popUpViewed', true)
+}
