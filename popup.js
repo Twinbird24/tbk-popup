20cc26bdc3595406f78cd38ca0b71644bdbc73be
Attila Komaromi <attila.kom@gmail.com>
2017-03-13 15:01:07 -0400

    Update js comments

--- popup.js
+++ popup.js
@@ -8,24 +8,10 @@ setTimeout( (function() {
 	});
 	function parallaxScroll() {
 		var scrolledY = $('#pop-up-container').scrollTop();
+		// if you want to changed scroll speed, modify the multiplier (aka the *0.2 and *-0.2 numbers)
 		$('#chisel').css('top','+20' - (((scrolledY)*0.2)) + '%');
 		$('#shavings').css('top','+10' - (((scrolledY)*-0.2)) + '%');
 	}
 }), 1000);
 
 //$('#pop-up').fadeOut('fast');
-
-
-
-// after 3 boring seconds of viewing the website, give the user summin' nice to look at
-// setTimeout( (function(){
-// 	$('#pop-up-container').fadeIn('fast');
-// 	$('#pop-up-container').bind('scroll', function() {
-// 		console.log('scroll');
-// 	});
-// }), 1000);
-
-
-// $('#pop-up-container').on('scroll', function() {
-// 		console.log('scroll');
-// });
